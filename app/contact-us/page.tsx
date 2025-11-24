29fb5bb5311515280abb4625070062bc2d2a382a
Himanshu Pandey <devops@a3techideas.com>
2025-11-24 23:32:39 +0530

    UpdateLanding

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from "react";
+import LandingPage from "../page";
+
+export default function ContactUsPage() {
+  useEffect(() => {
+    const timeout = window.setTimeout(() => {
+      document
+        .getElementById("request-callback")
+        ?.scrollIntoView({ behavior: "smooth", block: "start" });
+    }, 200);
+
+    return () => window.clearTimeout(timeout);
+  }, []);
+
+  return <LandingPage />;
+}
+
